bf4f6c40eaef4df754662fa2667424db4e222380
Tiogaplanet <samuel.trassare@gmail.com>
2026-08-06 14:19:15 -0400

    Revise header comments in MPU_Queue.h
    Updated file header comments to include detailed description, authorship, and copyright information.

--- src/MPU_Queue.h
+++ src/MPU_Queue.h
@@ -1,22 +1,20 @@
-/* Copyright (C) 2018  Adam Green (https://github.com/adamgreen)
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-/* Circular queue used internally by MiP library. Overwrites oldest items once it is full.
-
-   NOT THREAD SAFE!
-   ****************
-*/
+/**
+ * @file MPU_Queue.h
+ * @brief Fixed-size circular queue (ring buffer) used internally by the MPU
+ *        library.
+ *
+ * @details This template class overwrites the oldest element when full. It is
+ * not thread-safe. Used for buffering clap, gesture, IR, and detection
+ * events.
+ *
+ * @author Adam Green (Original Author)
+ * @author Samuel Trassare (Maintainer)
+ * @copyright Copyright (C) 2018-2026 Samuel Trassare
+ * (https://github.com/Tiogaplanet) Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
 #ifndef MPU_QUEUE_H_
 #define MPU_QUEUE_H_
 
